9a843ef4f886087c2fdf79fcbc756dc16d75dfce
Hoang Huy <hoanghuy.gg96@gmail.com>
2020-01-24 16:08:31 +0700

    first commit

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "AIzaSyCpffFx5sZstG6aclYoEnodK4Nj5DF14F4";
+
+declare var google: any;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { let: number; lng: number } } }[];
+  status: "OK" | "ZORE_RESULTS";
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then(response => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: coordinates,
+        zoom: 16
+      });
+
+      new google.maps.Marker({ position: coordinates, map: map });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+form.addEventListener("submit", searchAddressHandler);
